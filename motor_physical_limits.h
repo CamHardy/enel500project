bcdb8d1fe1e8cfb5bc6291400392fe9818b8be24
Cameron Hardy <camfhardy@gmail.com>
2017-03-14 07:00:51 +0000

    Emergency backup of all files

--- motor_physical_limits.h
+++ motor_physical_limits.h
@@ -1,27 +1,43 @@
 // remember to mod(4096) the high value
-const uint16_t HEAD_MOTOR_LOW_VALUE =		1698;		//Motor ID 11
-const uint16_t HEAD_MOTOR_HIGH_VALUE =		4426;
+const uint16_t HEAD_MOTOR_LOW =				3330;		// Motor ID 11
+const uint16_t HEAD_MOTOR_HIGH =			150;
+const uint16_t HEAD_MOTOR_INIT =			4056;
 
-const uint16_t NECK_MOTOR_LOW_VALUE	=		2846;		//Motor ID 12
-const uint16_t NECK_MOTOR_HIGH_VALUE =		4914;
+const uint16_t NECK_MOTOR_LOW =				824;		// Motor ID 12
+const uint16_t NECK_MOTOR_HIGH =			2846;
+const uint16_t NECK_MOTOR_INIT =			3885;
 
-const uint16_t LA_SHOULDER_TIP_LOW =		1800;		//Motor ID 21
-const uint16_t LA_SHOULDER_TIP_HIGH	=		5121;
+const uint16_t LEFT_SHOULDER_TIP_LOW =		1800;		// Motor ID 21
+const uint16_t LEFT_SHOULDER_TIP_HIGH =		1030;
+const uint16_t LEFT_SHOULDER_TIP_INIT =		1800;
 
-const uint16_t LA_SHOULDER_LOW =			2613;		//Motor ID 22
-const uint16_t LA_SHOULDER_HIGH =			4096;	
+const uint16_t LEFT_SHOULDER_LOW =			2613;		// Motor ID 22
+const uint16_t LEFT_SHOULDER_HIGH =			4096;
+const uint16_t LEFT_SHOULDER_INIT =			2030;
 
-const uint16_t LA_ELBOW_LOW =				1925;		//Motor ID 24
-const uint16_t LA_ELBOW_HIGH =				3162;		
+const uint16_t LEFT_UPPER_ARM_LOW =			3069;		// Motor ID 23
+const uint16_t LEFT_UPPER_ARM_HIGH = 		1447;
+const uint16_t LEFT_UPPER_ARM_INIT =		1986;
 
-const uint16_t RA_SHOULDER_TIP_LOW =		2062;		//Motor ID 31
-const uint16_t RA_SHOULDER_TIP_HIGH	=		2891;
+const uint16_t LEFT_ELBOW_LOW =				1925;		// Motor ID 24
+const uint16_t LEFT_ELBOW_HIGH =			3162;
+const uint16_t LEFT_ELBOW_INIT =			2120;		
 
-const uint16_t RA_SHOULDER_LOW =			1594;		//Motor ID 32
-const uint16_t RA_SHOULDER_HIGH =			4096;	
+const uint16_t RIGHT_SHOULDER_TIP_LOW =		2110;		// Motor ID 31
+const uint16_t RIGHT_SHOULDER_TIP_HIGH =	2891;
+const uint16_t RIGHT_SHOULDER_TIP_INIT =	2110;
 
-const uint16_t RA_ELBOW_LOW	=				2024;		//Motor ID 34
-const uint16_t RA_ELBOW_HIGH =				4825;	
+const uint16_t RIGHT_SHOULDER_LOW =			1594;		// Motor ID 32
+const uint16_t RIGHT_SHOULDER_HIGH =		4096;
+const uint16_t RIGHT_SHOULDER_INIT =		2059;
+
+const uint16_t RIGHT_UPPER_ARM_LOW =		1026;		// Motor ID 33
+const uint16_t RIGHT_UPPER_ARM_HIGH =		2597;
+const uint16_t RIGHT_UPPER_ARM_INIT =		2048;
+
+const uint16_t RIGHT_ELBOW_LOW	=			2024;		// Motor ID 34
+const uint16_t RIGHT_ELBOW_HIGH =			764;
+const uint16_t RIGHT ELBOW_INIT =			1840;
 
 // mod(65536) for this one only (different motor in wheel mode)
 //const uint16_t WAIST_LOW_VALUE =			64936;		//Motor ID 43
